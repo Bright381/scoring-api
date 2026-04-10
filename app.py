aa9be55102bd3b2bbab34b30f685dd1ce5c2306a
Bright381 <remy.bright38@gmail.com>
2026-04-10 11:11:02 +0200

    Update app.py
    deleted if __name__ == "__main__": block

--- app.py
+++ app.py
@@ -68,10 +68,3 @@ def explore(sk_id: int):
         raise
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
